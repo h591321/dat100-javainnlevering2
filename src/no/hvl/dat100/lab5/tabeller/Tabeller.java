0c9c3f4ad5a7a038d8fdd8cb966e9d5060368c34
Birk <birk.johannessen@hotmail.com>
2020-10-21 09:57:29 +0200

    innleveringsklar

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -4,61 +4,86 @@ public class Tabeller {
 
 	// a)
 	public static void skrivUt(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
-
+		System.out.print("[ ");
+		for(int i:tabell) {
+			System.out.print(i+" ");
+		}
+		System.out.print("]");
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		String ut="[";
+		for(int i=0;i<tabell.length;i++) {
+			int tall=tabell[i];
+			ut+=Integer.toString(tall);
+			if(i<tabell.length-1) {ut+=",";}
+		}
+		ut+="]";
+		return ut;
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
+		int Sum=0;
+		for(int i:tabell) {
+			Sum+=i;
+		}
+		return Sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
+		boolean finns=false;
+		for(int i:tabell) {
+			if(i==tall) {finns=true;}
+		}
+		return finns;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
+		int index=-1;
+			for(int i=0;i<tabell.length;i++) {
+				if(tabell[i]==tall) {index=i;}
+			}
 
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
+		return index;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
-		
+		int[] llebat= new int[tabell.length];
+		int j=tabell.length-1;
+		for(int i=0;i<tabell.length;i++) {
+			llebat[j]=tabell[i];
+			j--;
+		}
+		return llebat;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		boolean sortert=true;
+		for(int i=0;i<tabell.length-1;i++) {
+			if(tabell[i]<=tabell[i+1]) {}
+				else {sortert=false;}
+			}
+		
+		return sortert;
 	}
-
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
+		int lengde=tabell1.length+tabell2.length;
+		int[] nyTab=new int[lengde];
+		for (int i=0;i<tabell1.length;i++) {
+			nyTab[i]=tabell1[i];
+		}
+		int k=0;
+		for(int j=tabell1.length;j<lengde;j++) {
+			nyTab[j]=tabell2[k];
+			k++;
+		}
+		return nyTab;
 	}
 }
--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -5,31 +5,58 @@ public class Matriser {
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int i[] : matrise) {
+			System.out.print("[");
+			for (int j : i) {
+				System.out.print(j+" ");
+			}
+			System.out.println("]");
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		String ut = "";
+		for (int r[] : matrise) {
+			for (int i : r) {
+				ut+= i + " ";
+			}
+			ut += "\n";
+		}
+		return ut;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
+		int[] lengde = matrise [0];
+		for (int i = 0; i < matrise.length ; i++) {
+			for (int k = 0 ; k< lengde.length; k++) {
+				matrise [i][k] = (matrise [i][k])*tall;
+			}
+		}
+		return matrise;
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		boolean sjekk = false;
+		if (a.length!=b.length) {return sjekk;}
+		int [] lengde1 = a[0];
+		int [] lengde2 = b[0];
+		if (lengde1.length!=lengde2.length) {return sjekk;}
+		sjekk = true;
+
+		for (int i = 0 ; i < a.length ; i++) {
+			for (int k = 0 ; k < lengde1.length; k++) {
+				if (a[i][k]!=b[i][k]) {
+					sjekk=false;
+				}
+			}
+		}
+		
+		return sjekk;
 	}
 	
 	// e)
